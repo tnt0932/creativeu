4d1166d192b6273d713247e5da6f650479c5c511
Troy Tucker <troynoahtucker@gmail.com>
2013-05-11 21:27:08 -0700

    Isotope on profile

--- index.php
+++ index.php
@@ -223,8 +223,6 @@ $db = new MySQL($dbconfig['host'], $dbconfig['user'], $dbconfig['password'], $db
           
     $(function(){
 
-        
-
         if (Cookies.get('hideForWeek')) {
             $('#marketing').show(); // TODO: Remove to hide marketing bar
         } else {
@@ -251,65 +249,64 @@ $db = new MySQL($dbconfig['host'], $dbconfig['user'], $dbconfig['password'], $db
 
     });
 
+    // Make masonry work with centered layout
+$.Isotope.prototype._getCenteredMasonryColumns = function() {
+    this.width = this.element.width();
 
-// Make masonry work with centered layout
-    $.Isotope.prototype._getCenteredMasonryColumns = function() {
-        this.width = this.element.width();
+    var parentWidth = this.element.parent().width();
 
-        var parentWidth = this.element.parent().width();
+              // i.e. options.masonry && options.masonry.columnWidth
+    var colW = this.options.masonry && this.options.masonry.columnWidth ||
+              // or use the size of the first item
+              this.$filteredAtoms.outerWidth(true) ||
+              // if there's no items, use size of container
+              parentWidth;
 
-                  // i.e. options.masonry && options.masonry.columnWidth
-        var colW = this.options.masonry && this.options.masonry.columnWidth ||
-                  // or use the size of the first item
-                  this.$filteredAtoms.outerWidth(true) ||
-                  // if there's no items, use size of container
-                  parentWidth;
+    var cols = Math.floor( parentWidth / colW );
+    cols = Math.max( cols, 1 );
 
-        var cols = Math.floor( parentWidth / colW );
-        cols = Math.max( cols, 1 );
+    // i.e. this.masonry.cols = ....
+    this.masonry.cols = cols;
+    // i.e. this.masonry.columnWidth = ...
+    this.masonry.columnWidth = colW;
+    };
 
-        // i.e. this.masonry.cols = ....
-        this.masonry.cols = cols;
-        // i.e. this.masonry.columnWidth = ...
-        this.masonry.columnWidth = colW;
-        };
+    $.Isotope.prototype._masonryReset = function() {
+    // layout-specific props
+    this.masonry = {};
+    // FIXME shouldn't have to call this again
+    this._getCenteredMasonryColumns();
+    var i = this.masonry.cols;
+    this.masonry.colYs = [];
+    while (i--) {
+    this.masonry.colYs.push( 0 );
+    }
+    };
 
-        $.Isotope.prototype._masonryReset = function() {
-        // layout-specific props
-        this.masonry = {};
-        // FIXME shouldn't have to call this again
-        this._getCenteredMasonryColumns();
-        var i = this.masonry.cols;
-        this.masonry.colYs = [];
-        while (i--) {
-        this.masonry.colYs.push( 0 );
-        }
-        };
+    $.Isotope.prototype._masonryResizeChanged = function() {
+    var prevColCount = this.masonry.cols;
+    // get updated colCount
+    this._getCenteredMasonryColumns();
+    return ( this.masonry.cols !== prevColCount );
+    };
 
-        $.Isotope.prototype._masonryResizeChanged = function() {
-        var prevColCount = this.masonry.cols;
-        // get updated colCount
-        this._getCenteredMasonryColumns();
-        return ( this.masonry.cols !== prevColCount );
-        };
+    $.Isotope.prototype._masonryGetContainerSize = function() {
+    var unusedCols = 0,
+    i = this.masonry.cols;
+    // count unused columns
+    while ( --i ) {
+    if ( this.masonry.colYs[i] !== 0 ) {
+    break;
+    }
+    unusedCols++;
+    }
+    return {
+      height : Math.max.apply( Math, this.masonry.colYs ),
+      // fit container to columns that have been used;
+      width : (this.masonry.cols - unusedCols) * this.masonry.columnWidth
+    };
+};
 
-        $.Isotope.prototype._masonryGetContainerSize = function() {
-        var unusedCols = 0,
-        i = this.masonry.cols;
-        // count unused columns
-        while ( --i ) {
-        if ( this.masonry.colYs[i] !== 0 ) {
-        break;
-        }
-        unusedCols++;
-        }
-    
-        return {
-          height : Math.max.apply( Math, this.masonry.colYs ),
-          // fit container to columns that have been used;
-          width : (this.masonry.cols - unusedCols) * this.masonry.columnWidth
-        };
-  };
 
     function divClose(trigger, target) {
         $(trigger).click(function() {
